24205f1b315c6de3a029e66dfbfa53d02cc5554c
luojiafeng <526971157@qq.com>
2020-03-21 02:21:09 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- 头部 -->
+    <mt-header fixed title="JaFeng Luo"></mt-header>
+    <!-- 主体 -->
+4545
+    <!-- 底部 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item mui-active" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+    components: {
+    HelloWorld
   }
 }
+</script>
+<style lang="less">
+#app{
+  padding-top: 40px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+//使用mint-ui 引入全部组件
+import {Tabbar,Swipe,Header,Button,TabItem,SwipeItem} from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(Header.name, Header);
+Vue.component(Button.name, Button);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+//引入mit-ui全局样式
+import 'mint-ui/lib/style.css'
+
+
+// mui样式
+import './lib/mui/css/mui.min.css'
+// 拓展图标
+import './lib/mui/css/icons-extra.css'
 
 new Vue({
   router,
